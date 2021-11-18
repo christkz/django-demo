159211a1129c8176e8a5379e9f81e24461fc5347
christkz <kezakimanachrist2@gmail.com>
2021-11-18 16:28:52 +0200

    create models Category,Product,Customer

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    cat_name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "categories"
+
+    def __str__(self):
+        return self.cat_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=45)
+    unit_price = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
+
+
+# Customer model
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    tel = models.CharField(max_length=45)
+    address = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
